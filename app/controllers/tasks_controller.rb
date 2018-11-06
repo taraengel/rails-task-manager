b81574f931c6216ee543fa184978f2dfc7ab352e
Tara_Engelhardt taraengy@gmail.com <taraengy@gmail.com>
2018-11-06 16:59:45 +0000

    rails task manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,45 @@
+class TasksController < ApplicationController
+
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
+    def index
+      @tasks = Task.all
+      end
+
+      def show
+        @task = Task.find(params[:id])
+      end
+
+      def new
+        @task = Task.new
+      end
+
+      def create
+        Task.create(task_params)
+        redirect_to tasks_path
+      end
+
+      def edit
+      end
+
+      def update
+        @task.update(task_params)
+        redirect_to task_path(@task)
+      end
+
+      def destroy
+        @task.destroy
+        redirect_to tasks_path
+      end
+
+      private
+
+      def task_params
+        params.require(:task).permit(:title, :details, :completed)
+      end
+
+      def find_task
+        @task = Task.find(params[:id])
+      end
+
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,30 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+# Read all restaurants
+  get 'tasks', to: 'tasks#index'
+
+  # Read one restaurant
+
+  # Create (2 requests)
+
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+
+  post 'tasks', to: 'tasks#create'
+
+  # Update (2 requests)
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Detroy
+
+  delete 'tasks/:id', to: 'tasks#destroy'
+
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
 end
+
+
